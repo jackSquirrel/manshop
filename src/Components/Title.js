dd7f0f402d85a7159bc6cdad59b4ac5826a93a73
Timur Ismagilov <ismagilov42@yandex.ru>
2020-10-28 14:41:01 +0300

    title section done

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -5,9 +5,15 @@ function Title() {
         <section className="title">
             <div className="title__logo">
                 <h1 className="title__title">porten</h1>
-                <br />
                 <p className="title__subtitle">Санкт-Петербург</p>
             </div>
+            <p className="title__text">Lorem ipsum dolor sit amet, consectetur 
+                adipiscing elit. Purus interdum purus, est tortor pulvinar ut in. 
+                Fringilla a diam enim sed justo, sed iaculis sagittis. Tortor id eu 
+                interdum nec ut iaculis. Penatibus ullamcorper ultricies morbi ipsum
+                sem metus pharetra, mi. Tortor nibh magna feugiat id nunc, dui nisl 
+                viverra.
+            </p>
         </section>
     )
 }
